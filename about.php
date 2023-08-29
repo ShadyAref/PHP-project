cd60e12c77615c37878cbe6063be110f8f8de527
ShadyAref <shadyaref10@gmail.com>
2023-08-29 13:14:57 +0300

    feat: about page with dummy data in it

---
+++ about.php
@@ -0,0 +1,22 @@
+<?php include "header.php" ?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<style>
+    .dummy-data {
+        margin-top: 200px;
+    }
+</style>
+<body>
+    <div class="container align-items-center">
+        <h1 class="dummy-data">THIS IS SUCH A DUMMY DATA :)</h1>
+    </div>
+</body>
+</html>
+
+<?php include "footer.php" ?>
